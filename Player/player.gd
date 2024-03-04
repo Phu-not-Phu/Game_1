b4391c23ad7e2ed3d4cfd3f6005e7cd1cbe10cc0
Phu-not-Phu <danghomp@gmail.com>
2024-03-04 16:25:58 +0700

    Add Double Jump

--- Player/player.gd
+++ Player/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody3D
 
+signal pressed_jump(jump_state: JumpState)
 signal set_movement_state(_movement_state: MovementState)
 signal set_movement_direction(_movement_direction: Vector3)
 
+@export var max_air_jump: int = 1
 @export var movement_states: Dictionary
+@export var jump_states: Dictionary
 
+var air_jump_count: int = 0
 var movement_direction: Vector3
-
 
 func _input(event):
 	if event is InputEventKey:
-		if event.as_text() == "W" || event.as_text() == "S" || event.as_text() == "A" || event.as_text() == "D" || event.as_text() == "Shift" || event.as_text() == "Space":
+		if event.as_text() == "W" || event.as_text() == "S" || event.as_text() == "A" || event.as_text() == "D" || event.as_text() == "Shift" || event.as_text() == "Space" || event.as_text() == "Ctrl":
 				if event.pressed: 
 					get_node("Status/" + event.as_text()).color = Color("ff6666")
 				else:
@@ -34,7 +37,7 @@ func _input(event):
 func _ready():
 	set_movement_state.emit(movement_states["stand"])
 	var anim_player = $Model/Barbarian/AnimationPlayer
-	var animations = ['Idle', 'Running_A', 'Running_B', 'Walking_A', 'Walking_B']
+	var animations = ['Idle', 'Running_A', 'Running_B', 'Walking_A', 'Walking_B', 'Jump_Idle']
 	
 	for animation in animations:
 		animation = anim_player.get_animation(animation)
@@ -43,6 +46,21 @@ func _ready():
 func _physics_process(delta):
 	if is_movement_ongoing():
 		set_movement_direction.emit(movement_direction)
+		
+	if is_on_floor():
+		air_jump_count = 0
+	elif air_jump_count == 0:
+		air_jump_count = 1
+	
+	if air_jump_count <= max_air_jump:
+		if Input.is_action_just_pressed("jump"):
+			var jump_name: String = "ground_jump"
+			
+			if air_jump_count > 0:
+				jump_name = "air_jump"
+			
+			pressed_jump.emit(jump_states[jump_name])
+			air_jump_count += 1
 
 func is_movement_ongoing() -> bool:
 	return abs(movement_direction.x) > 0 or abs(movement_direction.z) > 0
